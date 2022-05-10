458bceb14c2f8a3c55fb39415bce1cc7ed98f3bc
Giulia-92 <CIONCO.GIULIA@GMAIL.COM>
2022-05-10 16:43:04 +0200

    vue-boolflix

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <HeaderContent/>
+  </div>
+</template>
+
+<script>
+import HeaderContent from './components/HeaderContent.vue'
+
+export default {
+  name: 'App',
+  components: {
+   HeaderContent
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HeaderContent.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <ul>
+      <li>{{id}}</li>
+      <li>{{linguage}}</li>
+      <li>{{title}}</li>
+      <li>{{  }}</li>
+      <li>{{  }}</li>
+    </ul>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderContent',
+  props: {
+    id: Number,
+    linguage:String,
+    title:String
+  },
+  data(){
+    return{
+      apiPath:'https://api.themoviedb.org/3/search/movie?api_key=b1114bdb80acbfdef0b6a3f26eab00e2&query=ritorno+al+futuro'
+    }
+  },
+  created(){
+       console.log(this.apiPath)
+      
+  }
+}
+</script>
+
+
+
+
+"id": 105,
+            "original_language": "en",
+            "original_title": "Back to the Future",
+            "overview":
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
